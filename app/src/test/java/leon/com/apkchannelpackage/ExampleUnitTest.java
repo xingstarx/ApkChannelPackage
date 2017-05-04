f4b3642cbfe4020bdb954165d13347d570f19b2d
xingxing <xxx823952375@gmail.com>
2017-05-04 16:42:33 +0800

    本地maven运行

--- app/src/test/java/leon/com/apkchannelpackage/ExampleUnitTest.java
+++ app/src/test/java/leon/com/apkchannelpackage/ExampleUnitTest.java
@@ -2,6 +2,14 @@ package leon.com.apkchannelpackage;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +21,21 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testFindFile() throws IOException {
+        File file = new File("baseApk/v1-base-debug.apk");
+        JarFile jarFile = new JarFile(file);
+        JarEntry jarEntry = jarFile.getJarEntry("META-INF/MANIFEST.MF");
+
+        Enumeration<JarEntry> entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            if (entry.getName().endsWith(".SF")) {
+                System.out.println("entry.getName() ==" + entry.getName() + ", entry == " + entry);
+            }
+        }
+        System.out.println("file.exists() ==" + file.exists());
     }
 }
